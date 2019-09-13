36192575fb8b3cd2c7ce575efed23ac4e8f57c97
Jake Tilk <tilkjacob@gmail.com>
2019-09-13 14:31:16 -0400

    fixing-backend

--- server.js
+++ server.js
@@ -3,17 +3,17 @@ const cors = require("cors");
 const app = express();
 const router = require("./routes");
 const itinerary = require("./itinerary.api");
-const pool = require("./connections"); //added after connection.js set-up
+// const pool = require("./connections"); //added after connection.js set-up
 // const googleMapsClient = require("@google/maps").createClient({
 //   key: "AIzaSyAD2FKgCts12KdWq0lVY7TJjEcP4e7RkoU",
 //   Promise: Promise
 // });
 
 // app.use(express.static(__dirname + "/dist"));
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 app.use("/", router);
 app.use("/", itinerary);
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT;
 app.listen(port, () => console.log(`Server running on PORT:  ${port}!`));
