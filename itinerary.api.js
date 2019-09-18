774d83672528b6f400d92399f2816dd87324c082
kevin balboa <kpbalboa@gmail.com>
2019-09-18 15:20:17 -0400

    add directions

--- itinerary.api.js
+++ itinerary.api.js
@@ -22,14 +22,13 @@ itinerary.delete("/itinerary/:id", (req, res) => {
 itinerary.post("/itinerary", (req, res) => {
   pool
     .query(
-      "insert into Itinerary (name, opening_hours, rating, vicinity, startdatetime, city) values ($1::text, $2::text, $3::numeric, $4::text, $5::text, $6::text)",
+      "insert into Itinerary (name, opening_hours, rating, vicinity, startdatetime) values ($1::text, $2::text, $3::numeric, $4::text, $5::text)",
       [
         req.body.name,
         req.body.opening_hours,
         req.body.rating,
         req.body.vicinity,
         req.body.startdatetime,
-        req.body.city
       ]
     )
     .then(() => {
